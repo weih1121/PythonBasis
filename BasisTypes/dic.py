0d633c8f0af83e040758bcd3f2471efac5416601
WH <2586898626@qq.com>
2018-03-19 23:40:46 +0800

    dict未完成

--- BasisTypes/dic.py
+++ BasisTypes/dic.py
@@ -5,7 +5,26 @@
 #区别在于:字典当中的元素是通过键来存取的，而列表是通过便宜存取的
 
 #8.2创建字典:
-dict1 = {'name':"alex", 'age':36, 'sex':'male'}
-dict2 = dict((('name', 'alex'), ))
-print(dict1)
-print(dict2)
+dict0 = {}          #创建空字典
+dict3 = dict()      #创建空字典
+dict1 = {'name':"alex", 'age':36, 'sex':'male'}                 #通过键值对直接构造字典
+dict2 = dict((('name','alex'), ('age',15), ('sex', 'male')))    #通过键值对和dict()构造函数构造字典
+
+names = ['a', 'b', 'c', 'd']
+ages = [10, 15, 18, 0]
+dict4 = {name : age for name, age in zip(names, ages)}          #通过迭代器生成字典
+
+
+# for name, age in zip(names, ages):
+#     dict[name] = age
+
+#增
+dict5 = {}
+dict5['name'] = 'alex'
+dict5['age']  = 18
+print(dict5)
+
+a = dict5.setdefault('name', 'hei')
+b = dict5.setdefault('age', 88)
+print(a, b)
+print(dict5)
