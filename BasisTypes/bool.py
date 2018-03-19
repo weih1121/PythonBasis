93d81066c0205080d61e522098698481f2d9e8e9
weih1121 <2586898626@qq.com>
2018-03-19 16:10:47 +0800

    添加布尔值

---
+++ BasisTypes/bool.py
@@ -0,0 +1,13 @@
+#5. 布尔值bool True or False
+#5.1
+print(True)
+print(False)
+print(bool([3, 4]))
+print(True + 1)     #布尔值非零即一
+
+#5.2 与或非操作
+
+print(bool(1 and 0))    #与
+print(bool(1 and 1))    #与
+print(bool(1 or 0))     #或
+print(bool(not 0))      #非
